3a5ea688b5f99acb4dbca01d122bc7a60d259e92
Pavel Moukhataev <pmoukhataev@pulsepoint.com>
2021-04-19 17:03:45 +0300

    Settings

---
+++ app/src/main/java/org/mpashka/findme/ui/settings/SettingsFragment.java
@@ -0,0 +1,14 @@
+package org.mpashka.findme.ui.settings;
+
+import android.os.Bundle;
+
+import androidx.preference.PreferenceFragmentCompat;
+
+import org.mpashka.findme.R;
+
+public class SettingsFragment extends PreferenceFragmentCompat {
+    @Override
+    public void onCreatePreferences(Bundle savedInstanceState, String rootKey) {
+        setPreferencesFromResource(R.xml.root_preferences, rootKey);
+    }
+}
